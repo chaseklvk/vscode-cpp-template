80d4dbfd1602fc5dca63a596db5c73ff209470de
Chase Zimmerman <chase@thezimmerman.net>
2017-10-09 11:55:22 -0700

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//*****************
+//Project Name:
+//Project Description:
+//Project Author:
+//Is this an extra credit Project:
+//Date completed:
+//Operating system used:
+//IDE Used:
+//*****************
+#include <iostream>
+#include "utilities.hpp"
+using namespace std;
+
+int main() {
+
+  cout << "Program ending, have a nice day!" << endl;
+  return 0;
+}
