194780bd4ef4033c170f6c80ab06b70ddb6206f3
Chase Zimmerman <chase@thezimmerman.net>
2017-10-11 13:45:48 -0700

    Simple multimap example

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,37 @@
 //IDE Used:
 //*****************
 #include <iostream>
+#include <map>
 #include "utilities.hpp"
 using namespace std;
 
 int main() {
 
-  cout << "Program ending, have a nice day!" << endl;
-  return 0;
+  multimap<string, double> investmentAccounts;
+
+  multimap<string, double>::iterator mmIterator;
+
+  pair<multimap<string, double>::iterator, multimap<string, double>::iterator> pairIterator;
+
+  investmentAccounts.insert(pair<string, double>("456", 100000.00));
+  investmentAccounts.insert(pair<string, double>("456", 200000.00));
+  investmentAccounts.insert(pair<string, double>("456", 300000.00));
+  investmentAccounts.insert(pair<string, double>("123", 400000.00));
+  investmentAccounts.insert(pair<string, double>("789", 500000.00));
+
+  pairIterator = investmentAccounts.equal_range("456");
+
+  if (pairIterator.first == pairIterator.second) {
+    cout << "Not found in the multimap" << endl;
+  } else {
+    for (
+      mmIterator = pairIterator.first; // Starts at first as defined by the equal_range method above
+      mmIterator != pairIterator.second; // Defined as the first node outside the range (789 for the example)
+      mmIterator++
+    )
+      cout << mmIterator->first << ", " << mmIterator->second << endl;
+
+    cout << "Program ending, have a nice day!" << endl;
+    return 0;
+  }
 }
